3b305dd39c48e2ee828de82dc5697a491febc757
LeeYeongGeun <91305949+LeeYeongGeun@users.noreply.github.com>
2022-03-13 18:46:36 +0900

    django course-60

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,12 @@ RUN pip install -r requirements.txt
 
 RUN echo "SECRET_KEY=django-insecure-se-elw^!*xkw0q2umekrv#+*m8a+(ny(g__x^!^9@_&2ehw_b!" > .env
 
+RUN pip install gunicorn
+
 RUN python manage.py migrate
+
+RUN python manage.py collectstatic
 
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["gunicorn", "config.wsgi", "--bind", "0.0.0.0:8000"]
--- config/settings/base.py
+++ config/settings/base.py
@@ -19,27 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -90,15 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
